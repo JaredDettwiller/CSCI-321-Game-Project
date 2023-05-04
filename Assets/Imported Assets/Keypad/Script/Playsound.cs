e40fd08b4203a591210fe2f01eb98e1c1aeb26b8
JaredDettwiller <Blitzstriker45@gmail.com>
2023-05-04 14:16:34 -0700

    Added first 33% of imported assets

---
+++ Assets/Imported Assets/Keypad/Script/Playsound.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playsound : MonoBehaviour 
+
+{
+	public void Clicky (){
+		GetComponent<AudioSource>().Play();
+		pressButton();
+	}
+
+	public void pressButton()
+    {
+		string buttonName = gameObject.name;
+		GameObject keyPad = GameObject.Find("Keypad");
+
+		switch (buttonName)
+        {
+			case "Enter":
+				// TODO: Enter the component
+				string correctString = "404";
+				if(keyPad.GetComponent<Keypad>().enteredIntoKeypad.Equals(correctString))
+                {
+					// Get rid of the barrier
+					GameObject barrier = GameObject.Find("EnterWaveTwoTrigger");
+					barrier.GetComponent<Collider>().isTrigger = true;
+					Light light = GameObject.Find("Crystal Light").GetComponent<Light>();
+					light.range = 10;
+
+					deactivateKeypad(keyPad);
+					// Text
+					GameObject textBox = GameObject.Find("Textbox");
+					textBox.GetComponent<TextboxTrigger>().DisplayTextInPanel("CorrectCodeText");
+				}
+				else
+                {
+					deactivateKeypad(keyPad);
+					// Text
+					GameObject textBox = GameObject.Find("Textbox");
+					textBox.GetComponent<TextboxTrigger>().DisplayTextInPanel("WrongCodeText");
+				}
+				break;
+			case "Clear":
+				deactivateKeypad(keyPad);
+				break;
+			default:
+				keyPad.GetComponent<Keypad>().addNumberToString(buttonName);
+				break;
+
+		}
+    }
+
+	private void deactivateKeypad(GameObject keyPad)
+    {
+		keyPad.GetComponent<Keypad>().clearString();
+		keyPad.SetActive(!keyPad.activeInHierarchy);
+		Time.timeScale = 1f;
+	}
+
+
+}
